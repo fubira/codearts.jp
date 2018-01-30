7658dd69356d2db0aecb7cdbd25153e6003a81ad
MikiSayaka <sayakadayo@tehepero.com>
2018-01-31 06:34:37 +0900

    bug fix

--- js/codearts.js
+++ js/codearts.js
@@ -3,23 +3,22 @@ $(function initialSet(){
 
 });
 
-$(function animationIndex(){
-	$(window).on("load", function(){
-		$('header').animate({
-			height:"60px"
+//animationIndex
+$(window).on("load", function(){
+	$('header').animate({
+		height:"60px"
+	},300,'easeOutBack');
+
+	setTimeout(function(){
+		$('header #title').animate({
+			left:"40px"
 		},300,'easeOutBack');
+	},100);
 
-		setTimeout(function(){
-			$('header #title').animate({
-				left:"40px"
-			},300,'easeOutBack');
-		},100);
-
-		setTimeout(function(){
-			$('.top_word').animate({
-				opacity:1,
-				bottom:"150px"
-			},500,'easeOutSine');
-		},300);
-	});
+	setTimeout(function(){
+		$('.top_word').animate({
+			opacity:1,
+			bottom:"150px"
+		},500,'easeOutSine');
+	},300);
 });
